4cab7f984fefef400ebeb6abdafb5ccd61d69fcb
Prasad Patil <prpatil42@gmail.com>
2015-04-09 12:39:29 -0400

    Added code for covariate adjustment without decision tree modeling

--- genesigprecision_run.R
+++ genesigprecision_run.R
@@ -18,9 +18,9 @@ set.seed(42341)
 # First, we generate an example decision tree- Figure 1 in the paper
 sampfun <- function(pd){ pd[sample(nrow(pd), replace=TRUE),]}
 pdtmp <- sampfun(pd) # We sample the data as per the scheme selected
-mod_clin_no_age <- rpart(y~Characteristics.TumorSize + Factor.Value.ER.status + g2ind + g3ind, data=pdtmp)
-mod_clin_no_age <- prune(mod_clin_no_age, cp=mod_clin_no_age$cptable[which.min(mod_clin_no_age$cptable[,"xerror"]),"CP"])
-drawTreeNodes(mod_clin_no_age)
+mod_clin_no_er <- rpart(y~Characteristics.Age + Characteristics.TumorSize + g2ind + g3ind, data=pdtmp)
+mod_clin_no_er <- prune(mod_clin_no_er, cp=mod_clin_no_er$cptable[which.min(mod_clin_no_er$cptable[,"xerror"]),"CP"])
+drawTreeNodes(mod_clin_no_er)
 
 # Next, we provide code to run the two simulations described- the complete label
 # permutation (Table 2) and the resampling simulation (Table 3). We also 
@@ -102,3 +102,38 @@ xtable(final, digits=5)
 
 1 - 1/(1+rot)
 1 - 1/(1+col)
+
+# This section will produce the resampling simulation results without decision-tree modeling of the covariates (for comparison)
+
+source("par_fun_nomod.R")
+
+reg <- makeRegistry(id="mrun_nomod")
+ids <- batchMap(reg, fun=par_fun_nomod, 40193:40292)
+
+# NOTE: This is the command that kicks off the batch jobs. You will need to wait until it is finished to load all of the results.
+done <- submitJobs(reg, wait=function(retries) 100, max.retries=10)
+
+# (not run) You may use this line to wait until the system has run all your
+# jobs before proceeding. Note that this may get stuck if a job fails or times out for
+# a cluster-related reason.
+
+# while(showStatus(reg)$done < showStatus(reg)$n){Sys.sleep(5)}
+
+y <- loadResults(reg)
+outmat <- do.call(rbind, y)
+means <- matrix(colMeans(outmat), 4, 3, byrow=T)
+vars <- apply(outmat, 2, var)
+col <- rot <- vector("numeric", 4)
+for(i in 1:4){
+	idx <- i + 2*(i-1)
+	rot[i] <- (vars[idx] - vars[idx + 1])/vars[idx]
+	col[i] <- (vars[idx] - vars[idx + 2])/vars[idx]
+}
+
+vars <- matrix(vars, 4, 3, byrow=T)
+
+final <- cbind(means[,1], vars[,1], means[,2], vars[,2], means[,3], vars[,3], rot, col)
+colnames(final) <- c("mu_una", "s_una", "mu_rot", "s_rot", "mu_col", "s_col", "g_rot", "g_col")
+rownames(final) <- c("no_age", "age", "gen", "cg")
+
+xtable(final, digits=5)
